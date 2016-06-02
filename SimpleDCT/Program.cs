2f322cfd426b477baf74fdc19fda7480fcc90df1
godka <asdic182@sina.com>
2016-06-03 02:33:04 +0800

    并行之后速度惊人

---
+++ SimpleDCT/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using drizzle.Algorithm;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+namespace SimpleDCT
+{
+    class MainClass
+    {
+        static List<trainelem> trainlist = new List<trainelem>();
+        public static void StartParallel(int index)
+        {
+            trainlist[index].Start();
+        }
+
+        public static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("train-images.idx3-ubyte", FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            br.ReadInt32();
+            var count = br.ReadInt32();
+            var w = br.ReadInt32();
+            var h = br.ReadInt32();
+            count = IPAddress.HostToNetworkOrder(count);
+            w = IPAddress.HostToNetworkOrder(w);
+            h = IPAddress.HostToNetworkOrder(h);
+
+            FileStream labelfs = new FileStream("train-labels.idx1-ubyte", FileMode.Open, FileAccess.Read);
+            BinaryReader labelbr = new BinaryReader(labelfs);
+            labelbr.ReadInt32();labelbr.ReadInt32();
+            Console.WriteLine("Init Data");
+            double[] double_list = new double[w * h];
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < w * h; j++)
+                    double_list[j] = br.ReadByte();
+                var mat = new Matrix(w, h, double_list);
+                var labelwrite = labelbr.ReadChar();
+                trainlist.Add(new trainelem(mat, labelwrite));
+            }
+
+            Console.WriteLine("Calc Data");
+            System.Threading.Tasks.Parallel.For(0, count - 1,
+                (i) => { StartParallel(i); });
+
+
+            Console.WriteLine("Write Data to Disk");
+            StreamWriter sw = new StreamWriter("train.txt");
+            sw.WriteLine(string.Format("{0} {1} {2}", 28 * 28, 10, 0));
+            foreach (var t in trainlist)
+            {
+                sw.Write(t.ToString());
+            }
+            sw.Close();
+            Console.WriteLine("All Done");
+        }
+    }
+}
---
+++ SimpleDCT/trainelem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using drizzle.Algorithm;
+namespace SimpleDCT
+{
+    public class trainelem
+    {
+        private Matrix _mat;
+        private int _result;
+        private string _str;
+        public trainelem(Matrix mat, int result)
+        {
+            _mat = mat;
+            _result = result;
+            _str = string.Empty;
+        }
+        public void Start()
+        {
+            var _tmp = Matrix.DCT(_mat);
+            _mat = _tmp.Normalize();
+            _str = _ToString();
+        }
+        public override string ToString()
+        {
+            return _str;
+            //return base.ToString();
+        }
+        private string _ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var t in _mat.GetData())
+            {
+                sb.Append(t);
+                sb.Append(",");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == _result)
+                {
+                    sb.Append("1");
+                }
+                else
+                {
+                    sb.Append("0");
+                }
+                sb.Append(",");
+            }
+            return sb.ToString();
+        }
+    }
+}
